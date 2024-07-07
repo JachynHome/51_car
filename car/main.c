7667af3c421020a2985b29d119137b19c265a55b
JachynHome <784217755@qq.com>
2024-07-07 19:53:16 +0800

    1.初次建立51_car工程

---
+++ car/main.c
@@ -0,0 +1,52 @@
+/**
+
+ * @file main.c
+
+ * @author Jachyn
+
+ * @brief 个人diy小车
+
+ * @version 0.1
+
+ * @date 2023-07-22
+
+ *
+
+ * @copyright Copyright (c) 2023
+
+ *
+
+ */
+
+
+
+#include <REGX52.H>
+
+
+
+/**
+
+ * @brief ???
+
+ *
+
+ */
+
+void main(){
+		P0_0=0;
+		P0_1=1;
+		P0_2=0;
+		P0_3=1;
+		P0_4=1;
+		P0_5=0;
+		P0_6=1;
+		P0_7=0;
+		P2_0=0;
+		P2_1=1;
+		P2_2=0;
+		P2_3=1;
+		P2_4=1;
+		P2_5=0;
+		P2_6=1;
+		P2_7=0;
+}
